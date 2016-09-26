25356d6993e237b5dde8e0dcef5e268291728689
edoarda <mavericktailsdoll@gmail.com>
2016-09-25 22:23:19 -0300

    Limpeza do código

--- substituicao.c
+++ substituicao.c
@@ -4,25 +4,27 @@
  #include <stdlib.h>
  #include <string.h>
 
+//funcao que pega um valor negativo e SOMA PRA CARAMBA até virar positivo porque funciona
 int soma_pra_caramba (int numero) {
     while (numero < 0)
         numero += 26;
     return numero;
 }
 
+//funcao magica que decripta E encripta
 char encryptifier(int *chave, char letra, int encriptar) {
-    //DE 65 A 90
-    //espaço é 32
-    //remover 64
+    //CHAR DO ASCII DE 65 A 90
     int chave_de_conta = soma_pra_caramba(*chave);
     char out;
     if (letra < 65 || letra > 90)
         return letra;
     else {
         out = ((soma_pra_caramba((letra-64) + chave_de_conta*encriptar)) % 26)+64; //se a letra resultado for Z vai dar 64 e nao 90, endireitar isso
-        if(out == 64)
-                 out = 90;
+        if(out == 64) //arruma caso a letra seja Z
+            out = 90;
     }
+
+    //coisador da chave nova
     if(encriptar==1)
         *chave = *chave + (out-64);
     else if (encriptar==-1)
@@ -33,13 +35,12 @@ char encryptifier(int *chave, char letra, int encriptar) {
 
  int main () {
      
-     char* para_a_musica = (char*) malloc (sizeof(char));//nao tem utilidade funcional nenhuma nesse programa, mas ta ai pela piada.
+     char* para_a_musica = (char*) malloc (sizeof(char)); //nao tem utilidade funcional nenhuma nesse programa, mas ta ai pela piada.
      int* chave = (int*) malloc (sizeof(int));
 
      //printf("#### (DES)CRIPTOVADOR 3000 ####\n\n");
 
      char escolha;
-     int chavinha;
 
      printf("Digite D para decriptar e E para encriptar\n");
      scanf("%c", &escolha);
@@ -54,25 +55,13 @@ char encryptifier(int *chave, char letra, int encriptar) {
     printf("Digite o valor da chave\n");
     scanf("%d", chave);
 
-     printf("Digite o texto\n ");
-
+     printf("Digite o texto\n");
+     fflush(stdin); //limpando o buffer pra não ter \n's alheios na saida
      char velha;
-
-     char saida;
      while (scanf("%c", &velha) && velha != '#') {
-         saida = encryptifier(chave, velha, encriptar);
-         printf("%c", saida);
+         velha = encryptifier(chave, velha, encriptar);
+         printf("%c", velha);
      }
-     /*if (escolha == 'e' || escolha == 'E') {
-         //encripta o maluco
-         //encryptifier(chavinha);
-     } 
-     else if (escolha == 'd' || escolha == 'D') {
-         //decripta o maluco
-     } 
-     else {
-         printf("Opcao invalida amigo. Tente novamente (Ou nao)");
-     }*/
 
      //I want to break
      free (para_a_musica);
